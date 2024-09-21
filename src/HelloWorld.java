c4bf0adf060348d927a87748587e0bb3219cabf5
Jasmina <jasminalee219@gmail.com>
2024-09-21 23:43:10 +0800

    init

---
+++ src/HelloWorld.java
@@ -0,0 +1,31 @@
+/*
+代码段注释
+ */
+
+/**
+ * 代码块注释
+ */
+
+// 行注
+/*
+ *The HelloWorld Application
+ */
+public class HelloWorld {
+    public static void main(String [] args)
+    {
+        int	radius;
+        radius = 3;
+
+        double	area = 0;
+        System.out.println("Radius = " + radius);
+        System.out.println("Area = " + area);
+
+        area = radius * radius * 3.14;
+    }
+
+}
+
+
+
+
+
